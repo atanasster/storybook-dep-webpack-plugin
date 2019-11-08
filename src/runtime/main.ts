74351baacc40d7a5a0912494b5738d6960c4a821
atanasster <atanasster@gmail.com>
2019-11-08 10:42:52 -0800

    export getDependencyMap function for direct access

--- src/runtime/main.ts
+++ src/runtime/main.ts
@@ -1,3 +1,19 @@
 import { IDependenciesMap } from './types';
 
-export const dependenciesMap:IDependenciesMap  = '__COMPILATION_HASH__INJECTED_DEPENDENCIES_DATA_PLACEHOLDER__' as any;
+export const dependenciesMap:string  = '__COMPILATION_HASH__INJECTED_DEPENDENCIES_DATA_PLACEHOLDER__';
+
+type DependencyStringifyFunction = () => IDependenciesMap;
+
+let map: IDependenciesMap = null;
+
+
+export const getDependencyMap: DependencyStringifyFunction = () => {
+  if (map) {
+    return map;
+  }
+  if (dependenciesMap) {
+    map = JSON.parse(dependenciesMap);
+    return map;
+  }
+  return undefined;
+}  
